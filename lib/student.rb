2a44d2ab4678451d417f499423a9a1c60b68e33a
Thomas Pope <tpope0928@gmail.com>
2019-01-07 01:46:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,16 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+  
+  def self.find_by(hash)
+    value = hash.values.first
+    edited_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys.first} = #{edited_value}"
+    DB[:conn].execute(sql)
+  end
 
 end
